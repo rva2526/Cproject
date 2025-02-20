76ad98b703c105b064d2b726f816c46b252db508
rva2526 <rvanalphen@usf.edu>
2025-02-20 19:48:28 +0000

    creating bash file to run all code

---
+++ run_prism.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Running Plouf based magnetics code'
+echo ''
+
+read -t 60 -p "Do you want to create a simple shape? (y/n) " create
+
+if ($create == 'y'){
+
+    echo 'creating...'
+}
