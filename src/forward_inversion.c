dbda29e6bec19302e9046c11d1eeb09c091d6b43
rva2526 <rvanalphen@usf.edu>
2025-03-04 21:32:00 +0000

    made inc and dec into radians for calculations

--- src/forward_inversion.c
+++ src/forward_inversion.c
@@ -53,9 +53,13 @@ double calculateVolumeIntegral(const struct Prism *prism, double px, double py) 
 
     }
 
-    double ml = cos(prism->minc) * cos(prism->mdec);
-    double mm = cos(prism->minc) * sin(prism->mdec);
-    double mn = sin(prism->minc);
+    double minc_rad,mdec_rad;
+    minc_rad = prism->minc*(PI/180);
+    mdec_rad = prism->mdec*(PI/180);
+
+    double ml = cos(minc_rad) * cos(mdec_rad);
+    double mm = cos(minc_rad) * sin(mdec_rad);
+    double mn = sin(minc_rad);
 
 
     double mx = prism->mi * ml;
